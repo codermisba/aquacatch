4c6160ce393d3cc05cda6c764b94869f4224dbfa
codermisba <misbas166@gmail.com>
2025-09-27 14:56:44 +0530

    fix: install Flutter before build in GitHub Actions 4

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ import 'theme.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: 'assets/.env');
   runApp(const MyApp());
 }
 
